afe0ca30dd1e25655272a90f9bb331313c45838b
joaquin <joacosampieri@gmail.com>
2023-08-08 20:07:03 -0300

    sprint 2

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -14,12 +18,16 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner init(ClientRepository clientRepository){
+	public CommandLineRunner init(ClientRepository clientRepository, AccountRepository accountRepository){
 		return args -> {
 			Client client = new Client("Melba","Morel","Melba@mindhub.com");
 			System.out.println(client);
 			clientRepository.save(client);
 			System.out.println(client);
+			Account account1 = new Account();
+			client.addAccount(account1);
+			accountRepository.save(account1);
 		};
+
 	}
 }
--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -2,10 +2,10 @@ package com.mindhub.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 public class Client {
     @Id
@@ -24,7 +24,16 @@ public class Client {
         lastName = last;
         email= mail;
     }
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    Set<Account> accounts = new HashSet<>();
 
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+    public void addAccount(Account account){
+        account.setOwner(this);
+        accounts.add(account);
+    }
     public String getFirstName() {
         return firstName;
     }
